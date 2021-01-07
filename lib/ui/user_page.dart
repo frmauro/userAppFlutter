7e32089a0d3bce07b95604d780dac9b1e6257cc5
francisco <frmauro8@hotmail.com>
2021-01-07 17:07:36 -0300

    imeplementation save user

--- lib/ui/user_page.dart
+++ lib/ui/user_page.dart
@@ -18,11 +18,18 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _userTypeController = TextEditingController();
   final _statusController = TextEditingController();
+
+  final _nameFocus = FocusNode();
+  final _emailFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+  final _userTypeFocus = FocusNode();
+  final _statusFocus = FocusNode();
 
   var _userTypesEnum = ['Administrator', 'Client'];
   var _statusEnum = ['Active', 'Inactive'];
@@ -46,7 +53,7 @@ class _UserPageState extends State<UserPage> {
       }),
     );
 
-    if (response.statusCode == 201) {
+    if (response.statusCode == 200) {
       // If the server did return a 201 CREATED response,
       // then parse the JSON.
       print(response.body);
@@ -87,21 +94,46 @@ class _UserPageState extends State<UserPage> {
 
           print("event onPressed");
 
-          print(_editedUser.name);
-          print(_editedUser.id);
-          print(_editedUser.email);
-          print(_editedUser.password);
-          print(_editedUser.userType);
-          print(_editedUser.status);
+          //print(_editedUser.name);
+          //print(_editedUser.id);
+          //print(_editedUser.email);
+          //print(_editedUser.password);
+          //print(_editedUser.userType);
+          //print(_editedUser.status);
+
+          if (_editedUser.name.isEmpty || _editedUser.name == null){
+            FocusScope.of(context).requestFocus(_nameFocus);
+          }
+
+          if (_editedUser.email.isEmpty || _editedUser.email == null){
+            FocusScope.of(context).requestFocus(_emailFocus);
+          }
+
+          if (_editedUser.password.isEmpty || _editedUser.password == null){
+            FocusScope.of(context).requestFocus(_passwordFocus);
+          }
+
+          if (_editedUser.userType.isEmpty || _editedUser.userType == null){
+            FocusScope.of(context).requestFocus(_userTypeFocus);
+          }
+
+          if (_editedUser.status.isEmpty || _editedUser.status == null){
+            FocusScope.of(context).requestFocus(_statusFocus);
+          }
 
           if (_editedUser.id == null){
             print("INSERT");
+            saveUser(_editedUser).then((result) {
+                 print("---- Result ----");
+                 print(result);
+                 Navigator.pop(context, _editedUser);
+            });
+
           } else {
             print("UPDATE");
           }
-          //saveUser(_editedUser).then((result) {
-            //print(result);
-          //});
+
+
         },
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
@@ -122,6 +154,7 @@ class _UserPageState extends State<UserPage> {
             ),
             TextField(
               controller: _nameController,
+              focusNode: _nameFocus,
               decoration: InputDecoration(labelText: "Name"),
               onChanged: (text) {
                 _userEdited = true;
